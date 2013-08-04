7a4d5e51e0b33a68039c9e6227c13f42056367b2
Sandeep <sandeep@ideophone.in>
2013-08-05 02:02:41 +0530

    Improvements in API for handling multi level, basic unit tests added

--- angellist/__init__.py
+++ angellist/__init__.py
@@ -8,9 +8,12 @@ ANGELLIST_API_URL = "https://api.angel.co/1/"
 
 class AngelList(object):
 	
-	def _request(self, endpoint, data, method="POST"):
+	def _request(self, endpoint, data, method="POST", _id=None):
 		if method in ["GET"]:
-			return json.loads(urllib2.urlopen("%s%s?%s" % (ANGELLIST_API_URL, endpoint, urlencode(data))).read())
+			if _id:
+				return json.loads(urllib2.urlopen("%s%s/%s?%s" % (ANGELLIST_API_URL, endpoint, _id, urlencode(data))).read())
+			else:
+				return json.loads(urllib2.urlopen("%s%s?%s" % (ANGELLIST_API_URL, endpoint, urlencode(data))).read())
 		if not self.access_token:
 			return {'error':'No access token'}
 		return json.loads(urllib2.urlopen("%s%s?access_token=%s&%s" % (ANGELLIST_API_URL, endpoint, self.access_token, urlencode(data))).read())
@@ -20,12 +23,14 @@ class AngelList(object):
 
 	def __getattr__(self, name):
 		def wrapper(data):
+			_id = None
 			if 'method' in data:
 				method = data['method'] 
 				del data['method']
 			else:
 				method = "POST"
-
-			response = self._request(name, data, method)
-			return response
+			if 'id' in data:
+				_id=data['id']
+				del data['id']
+			return self._request(name.replace("__","/"), data, method, _id)
 		return wrapper
